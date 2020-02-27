c793dea721c65c7337caf951c04a9fe2abdc91b8
Alana Bellucci <fastalana@gmail.com>
2020-02-27 16:23:07 -0700

    actually adding the test  eventListenergit add designs.js

--- designs.js
+++ designs.js
@@ -7,6 +7,14 @@ let height = document.getElementById('inputHeight');
 let width = document.getElementById('inputWidth');
 let table = document.getElementById('pixelCanvas');
 
+sizePicker.addEventListener('click', function(event){
+	event.preventDefault();
+	console.log(event + sizePicker.value);
+	// console.log(e + "test");
+	console.log(height.value, width.value);
+	console.log(colorPicker.value);
+});
+
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
 
