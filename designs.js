cf76390ad85374691985d3c790c54c5112fbfaa6
Alana Bellucci <alanabellucci@alanas-mbp.lan>
2020-02-27 16:13:29 -0700

    initializes elements with getElemementById and creates an event listener (.addEventListener) to make sure the elements are being captured correctly

--- designs.js
+++ designs.js
@@ -1,8 +1,13 @@
 // Select color input
+let colorPicker = document.getElementById('colorPicker');
+
 // Select size input
+let sizePicker = document.getElementById('sizePicker');
+let height = document.getElementById('inputHeight');
+let width = document.getElementById('inputWidth');
+let table = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
 
 // Your code goes here!
