3955cbcef0b8f9043eb10d4f0a2aa5969a468971
Alana Bellucci <fastalana@gmail.com>
2020-02-27 17:02:05 -0700

    cleanup

--- designs.js
+++ designs.js
@@ -11,19 +11,12 @@ makeGrid(height, width); //sets a default grid of 1x1
 
 sizePicker.addEventListener('click', function(event){
 	event.preventDefault();
-	table.firstChild.remove(); //removes the initial grid?
+	table.firstChild.remove(); //removes the initial grid
 	makeGrid(height, width);
-	// console.log(event + sizePicker.value);
-	// console.log(e + "test");
-	// console.log(height.value, width.value);
-	// console.log(colorPicker.value);
 });
 
 // When size is submitted by the user, call makeGrid()
 function makeGrid(height, width) {
-	// console.log(height.value, width.value);
-	// let row = table.insertRow(0);
-	// let cell = row.insertCell(0);
 	for(let x = 0; x < height.value; x++){
 		let row = table.insertRow(x);
 		for(let y = 0; y < width.value; y++){
@@ -34,8 +27,4 @@ function makeGrid(height, width) {
 			});
 		}
 	}
-
-
-// Your code goes here!
-
 }
