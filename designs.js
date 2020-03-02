1fe55f8f69c30782fb9f086eb293159f1851a566
Alana Bellucci <fastalana@gmail.com>
2020-03-02 07:05:04 -0700

    wrapping everything in a main function to take variables out of the global scope

--- designs.js
+++ designs.js
@@ -1,30 +1,33 @@
-// Select size input
-let height = document.getElementById('inputHeight');
-let width = document.getElementById('inputWidth');
-let table = document.getElementById('pixelCanvas');
+function main(){
+	// Select size input
+	let height = document.getElementById('inputHeight');
+	let width = document.getElementById('inputWidth');
+	let table = document.getElementById('pixelCanvas');
 
-makeGrid(height, width); //sets a default grid of 1x1
+	makeGrid(height, width); //sets a default grid of 1x1
 
+	//creates the grid when the user clicks on submit
+	document.querySelector('form').addEventListener('submit', function(event){ 
+		event.preventDefault();
+		table.firstChild.remove(); //removes the initial default grid
+		makeGrid(height, width);
+	});
 
-//creates the grid when the user clicks on submit
-document.querySelector('form').addEventListener('submit', function(event){ 
-	event.preventDefault();
-	table.firstChild.remove(); //removes the initial default grid
-	makeGrid(height, width);
-});
-
-// When size is submitted by the user, call makeGrid()
-function makeGrid(height, width) {
-	// Select color input
-	let colorPicker = document.getElementById('colorPicker');
-	for(let x = 0; x < height.value; x++){
-		let row = table.insertRow(x);
-		for(let y = 0; y < width.value; y++){
-			let cell = row.insertCell(y);
-			cell.addEventListener('click', function(event){
-				cell.style.backgroundColor = colorPicker.value;
-			});
+	// When size is submitted by the user, call makeGrid()
+	function makeGrid(height, width) {
+		// Select color input
+		let colorPicker = document.getElementById('colorPicker');
+		for(let x = 0; x < height.value; x++){
+			let row = table.insertRow(x);
+			for(let y = 0; y < width.value; y++){
+				let cell = row.insertCell(y);
+				cell.addEventListener('click', function(event){
+					cell.style.backgroundColor = colorPicker.value;
+				});
+			}
 		}
 	}
 }
+
+main()
 
